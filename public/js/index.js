3fb2babb73523ecb04f822b21ef8041568113b88
ankit18-259 <ankit18_259@aec.ac.in>
2020-09-12 04:27:02 +0530

    about page started and ping added on server

--- public/js/index.js
+++ public/js/index.js
@@ -17,3 +17,5 @@ var typed = new Typed('.type', {
                     // Default value
 
                   });
+
+
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+var health = require('express-ping');
 const express = require('express');
 const path = require('path');
 
 const app = express();
 PORT = process.env.PORT || 8080
-
+app.use(health.ping())
 app.use(express.static(path.join(__dirname, ('/public'))));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + ('/public/index.html'))));
+app.get('/about', (req, res) => res.sendFile(path.join(__dirname + ('/public/about.html'))));
+app.get('/timeline', (req, res) => res.sendFile(path.join(__dirname + ('/public/feedback.html'))));
+app.
 app.listen(PORT);
