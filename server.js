d61fa49e3264b6b8e852c68d1a24b00f203df1ac
ankit18-259 <ankit18_259@aec.ac.in>
2020-09-16 05:27:54 +0530

    design complete; review changes; added timeline html page

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ PORT = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, ('/public'))));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + ('/public/index.html'))));
 app.get('/about', (req, res) => res.sendFile(path.join(__dirname + ('/public/about.html'))));
-app.get('/timeline', (req, res) => res.sendFile(path.join(__dirname + ('/public/feedback.html'))));
+app.get('/timeline', (req, res) => res.sendFile(path.join(__dirname + ('/public/timeline.html'))));
 app.listen(PORT);
