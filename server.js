d3ec2861b72f4c6ca5e37980f85b284b0a536d56
ankit18-259 <ankit18_259@aec.ac.in>
2020-09-16 05:43:42 +0530

    button functionality added

--- server.js
+++ server.js
@@ -9,4 +9,5 @@ app.use(express.static(path.join(__dirname, ('/public'))));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + ('/public/index.html'))));
 app.get('/about', (req, res) => res.sendFile(path.join(__dirname + ('/public/about.html'))));
 app.get('/timeline', (req, res) => res.sendFile(path.join(__dirname + ('/public/timeline.html'))));
+app.get('/mentor_manual', (req, res) => res.sendFile(path.join(__dirname + ('/Data/Mentor_Manual.pdf'))));
 app.listen(PORT);
